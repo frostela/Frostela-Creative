1708e34ad1e944ee18a0529b6928621c2d64ac3c
Frostela <mainakdashlr@gmail.com>
2025-08-21 19:38:38 +0530

    addd website list, cat, sticking sliding effect

--- script.js
+++ script.js
@@ -5,30 +5,38 @@ const hoverSound = document.getElementById('hoverSound');
 hoverSound.volume = 0.3; // adjust volume here 🎵
 
 Array.from(hoverElements).forEach(element => {
-    element.addEventListener('mouseover', function () {
-        hoverSound.currentTime = 0; // restart from beginning
-        hoverSound.play();
-    });
+  element.addEventListener('mouseover', function () {
+    hoverSound.currentTime = 0; // restart from beginning
+    hoverSound.play();
+  });
 
-    // element.addEventListener('mouseleave', function () {
-    //     hoverSound.pause();
-    //     hoverSound.currentTime = 0; // reset to beginning
-    // });
+  // element.addEventListener('mouseleave', function () {
+  //     hoverSound.pause();
+  //     hoverSound.currentTime = 0; // reset to beginning
+  // });
 });
 
-gsap.registerPlugin(ScrollTrigger);
+// Sticky Section js
 
-gsap.from(".line", {
-  opacity: 0,
-  y: 40,
-  stagger: 0.2,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".sticky-section",
-    start: "top 70%",   // triggers earlier
-    end: "bottom 30%",
-    toggleActions: "play none none reverse", // simple play/reverse
-    markers: true       // turn on for debugging
+window.addEventListener('scroll', function () {
+  const myElement = document.getElementById('sticky-section-text');
+  const scrollPosition = window.scrollY;
+  const sound = document.getElementById('whoopSound');
+
+  if (scrollPosition > 200) {
+    if (!myElement.classList.contains('line-js')) {
+      sound.currentTime = 0; // restart sound
+      sound.play();
+    }
+    myElement.classList.add('line-js');
+    myElement.classList.remove('line');
+  } else {
+    if (!myElement.classList.contains('line')) {
+      sound.currentTime = 0;
+      sound.play();
+    }
+    myElement.classList.add('line');
+    myElement.classList.remove('line-js');
   }
 });
 
